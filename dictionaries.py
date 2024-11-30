0c684da6345068c955c828455e1923a09ed522be
Tyson Lanier <tslanier90@gmail.com>
2024-11-29 17:45:40 -0700

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -141,3 +141,91 @@ for language in set(favorite_languages.values()):
     print(language.title())
 for name in favorite_languages:
     print("Thank you for your response, " + name.title() + "!")
+
+# nesting examples (dictionary inside a list)
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
+
+# empty list for storing aliens
+aliens = []
+
+# make 30 green aliens
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+
+# show the first 5 aliens
+for alien in aliens[:5]:
+    print(alien)
+print("...")
+
+# show how many aliens have been created
+print(f"Total number of aliens: {len(aliens)}")
+
+# list inside a dictionary
+pizza = {
+    'crust': 'deep dish',
+    'toppings': ['cheese', 'sausage', 'bell peppers'],
+}
+
+# order summary
+print(f"You ordered a {pizza['crust']} pizza "
+      "with the following toppings:")
+
+for topping in pizza['toppings']:
+    print(f"\t{topping}")
+
+# another example
+favorite_languages = {
+    'jen': ['python', 'rust'],
+    'sarah': ['c', 'java'],
+    'edward': ['rust', 'go'],
+    'phil': ['c'],
+}
+for name, languages in favorite_languages.items():
+    if len(languages) == 1:
+        print(f"\n{name.title()}'s favorite language is:")
+        print(f"\t{languages[0].title()}")
+    else:
+        print(f"\n{name.title()}'s favorite languages are:")
+        for language in languages:
+            print(f"\t{language.title()}")
+
+# dictionary in a dictionary
+users = {
+    'aeinstein': {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+    },
+
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+    }
+}
+
+for username, user_info in users.items():
+    print(f"\nUsername: {username}")
+    full_name = f"{user_info['first']} {user_info['last']}"
+    location = user_info['location']
+
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tLocation: {location.title()}")
