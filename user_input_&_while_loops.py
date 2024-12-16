dc801efdaf4437c4ea5f0f4b6cfcb7e1291ec3bb
Tyson Lanier <tslanier90@gmail.com>
2024-12-16 15:52:03 -0700

    Update user_input_&_while_loops.py

--- user_input_&_while_loops.py
+++ user_input_&_while_loops.py
@@ -26,3 +26,27 @@ if number % 2 == 0:
     print(f"\nThe number {number} is even.")
 else:
     print(f"\nThe number {number} is odd.")
+
+# 7-1 Rental Car
+prompt = "What brand of car are you looking for today? "
+
+car = input(prompt)
+print(f"\nLet me see if I can find you a {car}!")
+
+# 7-2 Seating
+party = input("How many people are in your dinner group? ")
+party = int(party)
+
+if party > 8:
+    print("You will need to wait while we get a table ready.")
+else:
+    print("A table is ready for you now.")
+
+# 7-3 Multiples of Ten
+number = input("Enter a number, and I'll tell you if it's a multiple of ten: ")
+number = int(number)
+
+if number % 10 == 0:
+    print(f"\nThe number {number} is a multiple of ten.")
+else:
+    print(f"\nThe number {number} is not a multiple of ten.")
