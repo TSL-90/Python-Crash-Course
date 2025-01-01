fa956b5bd75193872d0bdb81b5c94173cd5c1382
Tyson Lanier <tslanier90@gmail.com>
2025-01-01 15:42:09 -0700

    Update user_input_&_while_loops.py

--- user_input_&_while_loops.py
+++ user_input_&_while_loops.py
@@ -50,3 +50,66 @@ if number % 10 == 0:
     print(f"\nThe number {number} is a multiple of ten.")
 else:
     print(f"\nThe number {number} is not a multiple of ten.")
+
+# while loops run as long as a condition is true
+current_number = 1
+while current_number <= 10:
+    print(current_number)
+    current_number += 1  # means current_number = current_number + 1
+
+# parrot program
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+message = ""
+while message != 'quit':
+    message = input(prompt)
+
+    if message != 'quit':
+        print(message)
+
+# using a Flag allows a program to stop running when a 'false' event occurs
+# flags are useful for complex programs
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+active = True  # active is the flag name, it can be anything you want
+while active:
+    message = input(prompt)
+
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
+
+# cities program
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\n(Enter 'quit' when you have finished.) "
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!")
+
+# counting
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue
+
+    print(current_number)
+
+# this loop runs forever! infinite loop
+x = 1
+while x <= 5:
+    print(x)
+
+# fixed
+x = 1
+while x <= 5:
+    print(x)
+    x += 1
