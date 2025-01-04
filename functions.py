780673f3186fb245832120387746233ea513665c
Tyson Lanier <tslanier90@gmail.com>
2025-01-03 21:46:57 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -19,3 +19,70 @@ def hi_user(username):
 
 
 hi_user('tyson')
+
+# 8-1 Message
+
+
+def display_message():
+    """Display the chapter in progress"""
+    print("I am learning chapter 8.")
+
+
+display_message()
+
+# 8-2 Favorite Book
+
+
+def favorite_book(title):
+    """Display the title of you favorite book."""
+    print(f"One of my favorite books is {title.title()}.")
+
+
+favorite_book("Alice in Wonderland")
+
+# Positional Arguments (order matters)
+
+
+def describe_pet(animal_type, pet_name):
+    """Display info about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+describe_pet('cockatiel', 'ether')
+describe_pet('cockatiel', 'nike')  # you can call a function multiple times
+
+# Keyword Arguments
+
+
+def describe_pet(animal_type, pet_name):
+    """Display info about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+describe_pet(animal_type='cockatiel', pet_name='ether')
+describe_pet(pet_name='nike', animal_type='cockatiel')
+
+# Default Value
+
+
+def describe_pet(pet_name, animal_type='bird',):  # still a positional argument
+    """Display info about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+describe_pet(pet_name='ether')
+
+# Equivalent Function Call (lets you overwrite a default argument)
+
+
+def describe_pet(pet_name, animal_type='bird',):  # still a positional argument
+    """Display info about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+describe_pet(animal_type='fish', pet_name='loach')
+describe_pet('loach', 'fish')  # both lines work the same
