eff454e5c28cedc3a7064ba515af268356688fdf
Tyson Lanier <tslanier90@gmail.com>
2025-01-06 14:39:14 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -196,3 +196,43 @@ while True:
 
     record = make_album(artist_name, album_name)
     print(f"\nYour favorite album is {record}.")
+
+# Passing a list
+
+
+def greet_users(names):
+    """Print a simgle greeting to each user in the list"""
+    for name in names:
+        msg = f"Hello, {name.title()}!"
+        print(msg)
+
+
+usernames = ['bob', 'tony', 'jerry', 'tom']
+greet_users(usernames)
+
+# Modifying a list in a function (3D print)
+
+
+def print_models(unprinted_designs, completed_models):
+    """
+    Simulate printing each design, until none are left.
+    Move each design to completed_models after printing.
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+
+
+def show_completed_models(completed_models):
+    """Show all the models that were printed."""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
