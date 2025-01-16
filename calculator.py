7c9618c825241d44d04f73c09c0439f68d2ed889
Tyson Lanier <tslanier90@gmail.com>
2025-01-16 11:29:45 -0700

    Updates
    Corrected code

--- calculator.py
+++ calculator.py
@@ -17,7 +17,7 @@ while not exit_calculator:
         print("You must input a valid number!")
         continue
 
-    while not exit_calculator:
+    if not exit_calculator:
         second_number = input("\nSecond number: ")
         if second_number.lower() == 'q':
             exit_calculator = True  # Set the flag to True to exit all loops
@@ -29,38 +29,38 @@ while not exit_calculator:
             print("You must input a valid number!")
             continue
 
-        while not exit_calculator:
-            operation = input("\nChoose an operation (+, -, *, /): ")
-            if operation.lower() == 'q':
-                exit_calculator = True  # Set the flag to True
+    if not exit_calculator:
+        operation = input("\nChoose an operation (+, -, *, /): ")
+        if operation.lower() == 'q':
+            exit_calculator = True  # Set the flag to True
+            # to exit all loops
+            break  # Exit the operation loop
+
+        if operation in ['+', '-', '*', '/']:
+            answer = None
+
+            try:
+                if operation == '+':
+                    answer = first_number + second_number
+                elif operation == '-':
+                    answer = first_number - second_number
+                elif operation == '*':
+                    answer = first_number * second_number
+                elif operation == '/':
+                    if second_number == 0:
+                        print("You can't divide by 0!")
+                        continue
+                    answer = first_number / second_number
+
+                print(f"{first_number} {operation} "
+                      f"{second_number} = {answer}")
+                # exit_calculator = True  # Set the flag to True
                 # to exit all loops
-                break  # Exit the operation loop
+                # break  # Exit the operation loop after printing the answer
 
-            if operation in ['+', '-', '*', '/']:
-                answer = None
+            except ZeroDivisionError:
+                print("You can't divide by 0!")
 
-                try:
-                    if operation == '+':
-                        answer = first_number + second_number
-                    elif operation == '-':
-                        answer = first_number - second_number
-                    elif operation == '*':
-                        answer = first_number * second_number
-                    elif operation == '/':
-                        if second_number == 0:
-                            print("You can't divide by 0!")
-                            continue
-                        answer = first_number / second_number
-
-                    print(f"{first_number} {operation} "
-                          f"{second_number} = {answer}")
-                    exit_calculator = True  # Set the flag to True
-                    # to exit all loops
-                    break  # Exit the operation loop after printing the answer
-
-                except ZeroDivisionError:
-                    print("You can't divide by 0!")
-
-            else:
-                print("Invalid operation, please choose +, -, *, /")
-                continue  # Continue to the operation input
+        else:
+            print("Invalid operation, please choose +, -, *, /")
+            continue  # Continue to the operation input
--- test_name_function.py
+++ test_name_function.py
@@ -4,6 +4,12 @@ from name_function import get_formatted_name
 
 
 def test_first_last_name():
-    """Do names like 'Tyson Lanier' work?"""
-    formatted_name = get_formatted_name('tyson', 'lanier')
-    assert formatted_name == 'Tyson Lanier'
+    """Do names like 'Wolfgang Mozart' work?"""
+    formatted_name = get_formatted_name('wolfgang', 'mozart')
+    assert formatted_name == 'Wolfgang Mozart'
+
+
+def test_first_last_middle_name():
+    """Do names like 'Wolfgang Amadeus Mozart' work?"""
+    formatted_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')
+    assert formatted_name == 'Wolfgang Amadeus Mozart'
